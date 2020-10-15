5ac520f4ec2a7e46295cccfbdc0c181fe2f18c51
Developer Prodigi <devprodigi2020@gmail.com>
2020-10-15 12:02:00 +0700

    change blade, new logo on sidebar

--- resources/views/layouts/dashboards/navbar/sidebar.blade.php
+++ resources/views/layouts/dashboards/navbar/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
       <li class="nav-item nav-profile">
-        <a href="#" class="nav-link">
+        {{-- <a href="#" class="nav-link">
           <div class="profile-image">
             <img class="img-xs rounded-circle" src="{{ asset('/src/assets/images/faces/face8.jpg')}}" alt="profile image">
             <div class="dot-indicator bg-success"></div>
@@ -10,11 +10,11 @@
             <p class="profile-name">Allen Moreno</p>
             <p class="designation">Premium user</p>
           </div>
-        </a>
+        </a> --}}
       </li>
       <li class="nav-item nav-category">Main Menu</li>
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="{{route('home')}}">
           <i class="menu-icon typcn typcn-document-text"></i>
           <span class="menu-title">Dashboard</span>
         </a>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,45 @@ Route::get('/', function () {
 // API City
 Route::get('/api/city/', 'InfograpikController@getCity');
 
+
+// Route::group(['prefix' => 'user'], function (){
+//     Route::get('login', 'PublicUsersController@formLogin')->name('users-login');
+//     Route::get('register', 'PublicUsersController@formRegister')->name('users-register');
+// });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Ketenagakerjaan
+Route::group(['prefix' => 'ketenagakerjaan'], function (){
+    Route::post('/update/{id}', 'KetenagakerjaanController@update')->name('update-ketenagakerjaan-admin');
+    Route::post('/store', 'KetenagakerjaanController@store')->name('store-ketenagakerjaan-admin');
+    Route::post('/delete/{id}', 'KetenagakerjaanController@destroy')->name('delete-ketenagakerjaan-admin');
+    Route::get('/create', 'KetenagakerjaanController@create')->name('create-ketenagakerjaan-admin');
+    Route::get('/index', 'KetenagakerjaanController@index')->name('index-ketenagakerjaan-admin');
+    Route::get('/edit/{judul}', 'KetenagakerjaanController@edit')->name('edit-ketenagakerjaan-admin');
+});
+
+// Infograpis
+Route::group(['prefix' => 'infograpik'], function (){
+    Route::post('/update/{id}', 'InfoGrapikController@update')->name('update-infograpik-admin');
+    Route::post('/store', 'InfoGrapikController@store')->name('store-infograpik-admin');
+    Route::post('/delete/{id}', 'InfoGrapikController@destroy')->name('delete-infograpik-admin');
+    Route::get('/create', 'InfoGrapikController@create')->name('create-infograpik-admin');
+    Route::get('/index', 'InfoGrapikController@index')->name('index-infograpik-admin');
+    Route::get('/edit/{judul}', 'InfoGrapikController@edit')->name('edit-infograpik-admin');
+});
+
+// Publikasi
+Route::group(['prefix' => 'publikasi'], function (){
+    Route::post('/update/{id}', 'PublikasiController@update')->name('update-publikasi-admin');
+    Route::post('/store', 'PublikasiController@store')->name('store-publikasi-admin');
+    Route::post('/delete/{id}', 'PublikasiController@destroy')->name('delete-publikasi-admin');
+    Route::get('/create', 'PublikasiController@create')->name('create-publikasi-admin');
+    Route::get('/index', 'PublikasiController@index')->name('index-publikasi-admin');
+    Route::get('/edit/{judul}', 'PublikasiController@edit')->name('edit-publikasi-admin');
+});
+
 // KATEGORI INFOGRAPIK DAN GALERY
 Route::group(['prefix' => 'kategori'], function (){
     Route::get('galery', 'Kategori\GaleryController@index')->name('index-kategori-galery');
@@ -37,43 +76,3 @@ Route::group(['prefix' => 'kategori'], function (){
     Route::post('infograpik/update/{id}', 'Kategori\InfograpikController@update')->name('update-kategori-infograpik');
     Route::post('infograpik/delete/{id}', 'Kategori\InfograpikController@destroy')->name('delete-kategori-infograpik');
 });
-
-// Publikasi
-Route::group(['prefix' => 'publikasi'], function (){
-    Route::post('/update/{id}', 'PublikasiController@update')->name('update-publikasi-admin');
-    Route::post('/store', 'PublikasiController@store')->name('store-publikasi-admin');
-    Route::post('/delete/{id}', 'PublikasiController@destroy')->name('delete-publikasi-admin');
-    Route::get('/create', 'PublikasiController@create')->name('create-publikasi-admin');
-    Route::get('/index', 'PublikasiController@index')->name('index-publikasi-admin');
-    Route::get('/edit/{judul}', 'PublikasiController@edit')->name('edit-publikasi-admin');
-});
-
-// Infograpis
-Route::group(['prefix' => 'infograpik'], function (){
-    Route::post('/update/{id}', 'InfoGrapikController@update')->name('update-infograpik-admin');
-    Route::post('/store', 'InfoGrapikController@store')->name('store-infograpik-admin');
-    Route::post('/delete/{id}', 'InfoGrapikController@destroy')->name('delete-infograpik-admin');
-    Route::get('/create', 'InfoGrapikController@create')->name('create-infograpik-admin');
-    Route::get('/index', 'InfoGrapikController@index')->name('index-infograpik-admin');
-    Route::get('/edit/{judul}', 'InfoGrapikController@edit')->name('edit-infograpik-admin');
-});
-
-// Ketenagakerjaan
-Route::group(['prefix' => 'ketenagakerjaan'], function (){
-    Route::post('/update/{id}', 'KetenagakerjaanController@update')->name('update-ketenagakerjaan-admin');
-    Route::post('/store', 'KetenagakerjaanController@store')->name('store-ketenagakerjaan-admin');
-    Route::post('/delete/{id}', 'KetenagakerjaanController@destroy')->name('delete-ketenagakerjaan-admin');
-    Route::get('/create', 'KetenagakerjaanController@create')->name('create-ketenagakerjaan-admin');
-    Route::get('/index', 'KetenagakerjaanController@index')->name('index-ketenagakerjaan-admin');
-    Route::get('/edit/{judul}', 'KetenagakerjaanController@edit')->name('edit-ketenagakerjaan-admin');
-});
-
-
-
-Route::group(['prefix' => 'user'], function (){
-    Route::get('login', 'PublicUsersController@formLogin')->name('users-login');
-    Route::get('register', 'PublicUsersController@formRegister')->name('users-register');
-});
-
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');

582d4556e348f0fe545ee663973119e80ef75cce
Developer Prodigi <devprodigi2020@gmail.com>
2020-10-21 21:49:28 +0700

    add field gambar on infograpik

--- database/migrations/2020_10_14_100422_create_info_grapiks_table.php
+++ database/migrations/2020_10_14_100422_create_info_grapiks_table.php
@@ -22,7 +22,6 @@ class CreateInfoGrapiksTable extends Migration
             $table->foreign('kategori_info_id')->references('id')->on('kategori_infos');
             $table->foreign('province_id')->references('id')->on('provinces');
             $table->foreign('city_id')->references('id')->on('cities');
-            $table->string('gambar');
             $table->timestamps();
         });
     }

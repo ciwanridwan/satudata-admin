3617b084bf4768664391030e9177e2757831e481
Developer Prodigi <devprodigi2020@gmail.com>
2020-11-02 14:37:02 +0700

    change title

--- resources/views/layouts/dashboards/app.blade.php
+++ resources/views/layouts/dashboards/app.blade.php
@@ -4,7 +4,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>SATUDATA - @yield('title')</title>
+  <title>Satudata - @yield('title')</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{asset('/src/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
   <link rel="stylesheet" href="{{asset('/src/assets/vendors/iconfonts/ionicons/dist/css/ionicons.css')}}">
